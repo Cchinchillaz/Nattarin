06b26b4f56660fe318c1028071807a3c85d43c8d
Nattarin Chetpattananondh <nattarinchet@gmail.com>
2024-12-26 14:23:05 +0700

    finish:story

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function Story(props) {
-  return (
-    <div className='w-full bg-black-300 flex justify-center'>
-      <div className="bg-white-800 rounded-xl w-[92vw] h-[90vw] sm:w-[72vw] sm:h-[72vw]  lg:w-[42vw] lg:h-[42vw] p-0 self-center fixed top-[55%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <button className='bg-black-300/50 p-2 rounded-sm' onClick={props.close}>close</button>
-      </div>
-    </div>
-  )
+function Story({ close, event }) {
+    return (
+        <div className="bg-black-300/90 border-2 border-white-800 rounded-xl w-[92vw] h-[70vh] sm:w-[72vw] sm:h-[62vh] lg:w-[42vw] lg:h-[62vh] p-0 self-center fixed top-[55%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <button onClick={close} className="text-neutral-400 hover:text-white-800" aria-label="Toggle menu">
+                <img src="assets/close.svg" alt="close" className="w-6 h-6 absolute right-2" />
+            </button>
+             <div className="w-[70%] mx-[15%] h-[50%] rounded-lg" style={{backgroundImage: `url('assets/exp/${event.id}.png')`, backgroundSize: 'cover'}}></div>
+             <div className='h-[50%]'>
+              <p className='grid-headtext mt-4 ml-4'>{event.title}</p>
+              <p className='grid-subtext mt-3 ml-4'>{event.desc}</p>
+             </div>
+        </div>
+    );
 }
 
-export default Story
+export default Story;
--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react'
-import { experiences } from '../constants'
+import React, { useState } from 'react';
+import { experiences } from '../constants';
 import Story from '../components/Story';
 
 function Experiences() {
-    const [isOpen,setIsOpen] = useState(false);
-    let story = null;
-    const toggleIsOpen = () => {setIsOpen(!isOpen)};
-    if(isOpen){
-        story = <Story close = {toggleIsOpen}/>
-    }
-    
+    const [isOpen, setIsOpen] = useState(false);
+    const [event, setEvent] = useState(null);
+
+    const toggleIsOpen = () => {
+        setIsOpen(!isOpen);
+        console.log(isOpen);
+    };
+
+    const handleEventClick = (exp) => {
+        setEvent(exp);
+        toggleIsOpen();
+    };
+
     return (
-    <section className='c-space my-10 w-full flex flex-col justify-center'>
-        <div className="grid-headtext w-full flex justify-center mb-2"> <p>select the chocolate to see the story!</p></div>
-        <div className="w-[90vw] h-[90vw] sm:w-[70vw] sm:h-[70vw]  lg:w-[40vw] lg:h-[40vw] p-0 self-center">
-            <div className="grid grid-cols-3 grid-rows-3 gap-0">
-                {experiences.map((exp) => (
-                    <div className="col-span-1 row-span-1 relative w-full pb-[100%]" key={exp.id}>
-                        <div className="w-full h-full absolute inset-0 cursor-pointer" style={{backgroundImage: `url('assets/box/${exp.id}.png')`, backgroundSize: 'cover'}}></div>
-                        <div className="absolute inset-0 bg-black/50 rounded-lg opacity-0 hover:opacity-60 cursor-pointer" onClick={toggleIsOpen}></div>
-                    </div>
-                ))}      
+        <section className='c-space my-10 w-full flex flex-col justify-center'>
+            <div className="grid-headtext w-full flex justify-center mb-2">
+                <p>Select the chocolate to see the story!</p>
             </div>
-        </div>
-        <div>{story}</div>
-    </section>
-  )
+            <div className="w-[90vw] h-[90vw] sm:w-[70vw] sm:h-[70vw] lg:w-[40vw] lg:h-[40vw] p-0 self-center">
+                <div className="grid grid-cols-3 grid-rows-3 gap-0">
+                    {experiences.map((exp) => (
+                        <div className="col-span-1 row-span-1 relative w-full pb-[100%]" key={exp.id}>
+                            <div className="w-full h-full absolute inset-0 cursor-pointer" style={{backgroundImage: `url('assets/box/${exp.id}.png')`, backgroundSize: 'cover'}}></div>
+                            <div className="absolute inset-0 bg-black/50 rounded-lg opacity-0 hover:opacity-60 cursor-pointer" onClick={() => handleEventClick(exp)}></div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={`transition-all duration-1000 ease-in-out ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
+                {isOpen && <Story close={toggleIsOpen} event={event} />}
+            </div>
+        </section>
+    );
 }
 
-export default Experiences
+export default Experiences;
